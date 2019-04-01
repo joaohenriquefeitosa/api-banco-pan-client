de6e822feae14f0a25320c5d5037320e3ac38ec9
Caio Russi <caio.russi@hotmail.com>
2019-04-01 10:59:12 -0300

    add phpunit

---
+++ src/Client.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Client
+ */
+class Client
+{
+
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    public function testAssertTrue(){
+        $this->assertEquals(true, true);
+    }
+}
