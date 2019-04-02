677051184c3c716866791b45ad41cddae1904013
Caio Russi <caio.russi@hotmail.com>
2019-04-02 10:47:21 -0300

    OV-1308 - Criar método de autenticação - Adicionado Classe BasicAuth responsável por realizar a autenticação e armazenar o token. - Adicionado a Classe HttpRequest responsável por realizar os requests http.

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,10 +1,26 @@
 <?php
 
+use Pan\Client;
 use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase
 {
-    public function testAssertTrue(){
-        $this->assertEquals(true, true);
+    private $client;
+
+    /**
+     * @throws Exception
+     */
+    public function setUp()
+    {
+        $this->client = new Client();
+    }
+
+    public function testAuthenticationSuccessfully() {
+        $result = $this->client->authenticate('01234567890_112358', 'Senha@1321', 'api-key');
+        $content = $result->getContent();
+
+        $this->assertNotEmpty($result);
+        $this->assertArrayHasKey("access_token", $content);
+        $this->assertArrayHasKey("data_hora_expiracao", $content);
     }
 }
